6298837ae68ca38fc362b6a310941351cda0a6bc
ldzabaleta <lucas@dezabaleta.com.ar>
2022-09-05 22:32:41 -0300

    add itemDetail

--- src/components/Views/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/Views/ItemDetailContainer/ItemDetailContainer.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import { useParams } from 'react-router-dom'
+import ItemDetail from "../ItemDetail/ItemDetail"
 import './ItemDetailContainer.css'
 
 
@@ -16,24 +17,8 @@ const ItemDetailContainer = () => {
     
 
     return (
-      < div className="text-light mb-4 image__zoom d-flex justify-content-center" >
-              
-        <div className="d-flex justify-content-around">
-            <div className="">
-            <img src={`../${item.image}`} alt={item.name} className="img-fluid"/>
-            </div>
-                
-            <div className="lead my-1">
-                <span className="text-decoration-none d-flex flex-column px-5">
-                    <h1 className="lead my-1 text-center fs-2 fw-bold">{item.name}</h1>
-                    <h3 className="lead my-1 text-center text__container">{item.description}</h3>
-                    <h1 className="lead my-1 text-center fs-4 fw-bold">${item.price}</h1>
-                    <span className="align-self-center py-3">
-                        <div> <button className="int__button" style={{lineHeight: '0.3rem'}}>COMPRAR</button></div>
-                    </span>
-                </span>
-            </div>
-        </div>
+    < div className="text-light mb-4 image__zoom d-flex justify-content-center" >
+        < ItemDetail item={item} />
     </div>
     )
   }
