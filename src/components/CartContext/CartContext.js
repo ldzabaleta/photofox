b1d2cb3e775c24cd6881a768e6e62ee50d9165c6
ldzabaleta <lucas@dezabaleta.com.ar>
2022-09-18 22:35:29 -0300

    cart

--- src/components/CartContext/CartContext.js
+++ src/components/CartContext/CartContext.js
@@ -1,34 +1,41 @@
 import React, {createContext, useContext, useState} from 'react'
 
-export const ProductsContext = createContext([]);
+export const ProductsContext = createContext({items: [], total: 0});
 
 export const UseProductsContext = () => useContext(ProductsContext);
 
 const ProductsProvider = ({ children }) => {
-    const [CartList, SetCartList] = useState([]);
+    const [CartList, SetCartList] = useState({items: [], total: 0});
     const AddProduct = (item, itemQuantity) => {
-      const index = CartList.findIndex((i) => i.id === item.id);
+      const index = CartList.items.findIndex((i) => i.id === item.id);
 
       if (index > -1 ) {  
-        CartList[index].cantidad += itemQuantity
+        CartList.items[index].cantidad += itemQuantity
+        CartList.total += itemQuantity * CartList.items[index].price
         SetCartList(CartList);
       } else {
         item.cantidad = itemQuantity
-        SetCartList([...CartList, item]);
+        CartList.total += item.cantidad * item.price
+        SetCartList({
+          items: [...CartList.items , item],
+          total: CartList.total
+        } );
       }
     };
 
     const ClearCart = () => {
-      SetCartList([]);
+      SetCartList({items: [], total: 0});
     };
 
     const RemoveProduct = (item) => {
-      const index = CartList.findIndex((i) => {
+      console.log('lalalalal')
+      const index = CartList.items.findIndex((i) => {
         return i.id === item.id
       });
 
         if (index > -1 ) {
-          CartList.splice(index, 1);
+          CartList.total -= CartList.items[index].cantidad * CartList.items[index].price
+          CartList.items.splice(index, 1);
           SetCartList(CartList); 
         }
       };
--- src/components/Views/Cart/Cart.js
+++ src/components/Views/Cart/Cart.js
@@ -1,11 +1,77 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { UseProductsContext } from '../../CartContext/CartContext'
+import DeleteIcon from '@mui/icons-material/Delete';
+import { Link } from 'react-router-dom';
 const Cart = () => {
-    const { CartList }  = UseProductsContext()
+    const { CartList, RemoveProduct }  = UseProductsContext()
+    const [cartList, setCartList] = useState(CartList)
+
+const DeleteItem = (item) => {
+    RemoveProduct(item)
+    setCartList(CartList)
+    console.log(CartList)
+    console.log(cartList);
+}
     console.log(CartList)
     return (
-        <div>Cart</div>
+        <div>
+            <table>
+                <tbody>
+                {cartList.items.length ? 
+                cartList.items.map((item, key) => (
+                    <tr key={key}>
+                        <td>
+                            <div className="px-3 pb-3"><img src={item.image} alt={item.name} width="48" /></div>
+                        </td>
+                        <td>
+                            <div className="align-middle px-sm-5"><h2 className="lead my-1">{item.name}</h2></div>
+                        </td>
+                        <td>
+                            <div className="align-middle lead">Cant. {item.cantidad}</div>
+                        </td>
+                        <td>
+                            <div className="px-4"><h2 className="lead my-1">${item.price}</h2></div>
+                        </td>
+                        <td>
+                            <div className="">
+                                <span>
+                                    <div className="p-2"> 
+                                        
+                                        <DeleteIcon onClick={() => DeleteItem(item)}></DeleteIcon>
+                                                
+                                                {/* onClick={removeProduct({index})} */}    
+                                    </div>
+                                </span>
+                            </div>
+                        </td>
+                    </tr>
+                ))
+            : 
+            <><tr>
+                        <td>El carrito está vacío</td>
+                    </tr><tr>
+                            <td>
+                                <Link to="/">
+                                    <div> <button className="int__button" style={{ lineHeight: '0.3rem' }}>SEGUIR COMPRANDO</button></div>
+                                </Link>
+                            </td>
+                        </tr></>
+            } 
+            {
+                cartList.items.length ? 
+                <tr>
+                    <td>
+                    <h5>Total</h5>
+                </td>
+                <td>
+                    ${cartList.total.toFixed(2)}
+                </td>
+            </tr>  : ''          }
+            </tbody>
+            </table>
+        </div>
     )
 }
+
 
 export default Cart;
